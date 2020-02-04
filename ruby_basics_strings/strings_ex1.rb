8be3f28c9f1b03395dd803e1a1589a5e17eef372
Christin Hebron <puilunchristin@gmail.com>
2020-02-04 21:39:53 +0000

    added string exercises

---
+++ ruby_basics_strings/strings_ex1.rb
@@ -0,0 +1,4 @@
+#1. Create an empty string using the String class and assign it to a variable.
+
+empty_string = string.new
+
---
+++ ruby_basics_strings/strings_ex8.rb
@@ -0,0 +1,6 @@
+#8. Using the following code, split the value of alphabet by individual characters and print each character.
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+puts alphabet.split('')
+
