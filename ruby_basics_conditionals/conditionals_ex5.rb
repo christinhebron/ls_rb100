b043561c1390b095d6f103ab55e7eadb54f8f59b
Christin Hebron <puilunchristin@gmail.com>
2020-02-04 18:46:04 +0000

    Added conditionals exercises

---
+++ ruby_basics_conditionals/conditionals_ex5.rb
@@ -0,0 +1,11 @@
+#5. What will the following code print? Why? Don't run it until you've attempted to answer.
+
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+#Answer: My favorite number is 7
